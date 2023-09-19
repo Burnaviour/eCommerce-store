79981b952b4674841355a82d54c838161e6209c5
Burnaviour <meharmuzafar760@gmail.com>
2023-09-19 17:59:07 +0500

    api client test added

--- store/test/test_collections.py
+++ store/test/test_collections.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APIClient
 from rest_framework import status
+from django.contrib.auth.models import User
 import pytest
 
 
@@ -15,3 +16,41 @@ class TestCreateCollection:
         })
         # assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_return_403(self):
+        # AAA (Arrange,Act,Assert)
+        # Arrange
+        client = APIClient()
+        # ACT
+        client.force_authenticate(user={})
+        response = client.post('/store/collection/', {
+            'title': 'a'
+        })
+        # assert
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_return_400(self):
+        # AAA (Arrange,Act,Assert)
+        # Arrange
+        client = APIClient()
+        # ACT
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collection/', {
+            'title': ' '
+        })
+        # assert
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_if_data_is_valid_return_201(self):
+        # AAA (Arrange,Act,Assert)
+        # Arrange
+        client = APIClient()
+        # ACT
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collection/', {
+            'title': 'a'
+        })
+        # assert
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
